e21d4f1dafe068a8eb520d1b5bc46f7a5c35b812
YanYan Lee <leeya@oregonstate.edu>
2022-02-20 02:36:53 -0800

    find available space in a page file

--- classes.h
+++ classes.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <sstream>
 #include <bitset>
+#include <math.h>
 using namespace std;
 
 class Record {
@@ -42,45 +43,98 @@ private:
     string fName; // index file name
     int block_size; // total occupied bytes
 
+    // hash id and convert to binary and return the integer key
+    long unsigned int create_key(int id){
+        long unsigned int h_value = id%(2^16);
+        bitset<8> all_bits(h_value);
+        bitset<8> b_key;
+        for(int j = 0; j < i; j++){
+             b_key.set(j, all_bits[j]);
+        }
+        //cout << " all key bits: "<< all_bits.to_string() << " " << i << " bits key: "<< b_key.to_string() << endl;
+
+        long unsigned int i_key = b_key.to_ulong();
+        cout <<  "index key: "<< i_key << endl;
+        return i_key;
+    }
+
+    // find available size of single block
+    int find_space(int start_index){
+        ifstream file;
+        file.open(fName, ifstream::in);
+        int index = -1;
+
+        for(int i = start_index; i < start_index + PAGE_SIZE; i++){
+            //cout << "i: "<< i << endl;
+            file.seekg(i);
+            //cout << file.tellg() << endl;
+            char c;
+            file.get(c);
+
+            cout << "["<< (int)c << "]" << endl;
+            //
+            if(c == '\0' || c == '$'){
+                cout << " found empty space at "<< i << endl;
+                index = i;
+                break;
+            }
+        }
+        file.close();
+        return index;
+    }
+
+
     // Insert new record into index
     void insertRecord(Record record) {
         ofstream index_file;
-        index_file.open(fName, ofstream::app);
+        index_file.open(fName);
 
         // No records written to index yet
         if (numRecords == 0) {
             // Initialize index with first blocks (start with 2)
-            pageDirectory.resize(2);
+            pageDirectory.resize(2, 0);
+            pageDirectory[1] = 4097;
             numBlocks = 2;
-            i++;
+
+            // set up record space
+            for(int j = 0; j < PAGE_SIZE*2 ;j++){
+                index_file << '$';
+            }
         }
+        // calculate bits to select for creating key
+        i = ceil(log(numBlocks));
+        long unsigned int i_key = create_key(record.id);
 
-        // Add record to the index in the correct block, creating overflow block if necessary
-        // hash id and convert to binary
-        long unsigned int h_value = record.id%(2^16);
-        bitset<8> all_bits(h_value);
-        bitset<8> b_key;
-// i=8;
-        // insert and convert i bits key to find bucket
-        for(int j = 0; j < i; j++){
-            b_key.set(j, all_bits[j]);
+        // available page in page directory
+        if(i_key <= numBlocks - 1){
+
+            // loop through record to find empty pos
+            int space = find_space(pageDirectory[i_key]);
+            cout <<  "space return: "<< space << endl;
+
+            // 4 symbols(4 bytes) + total size of record
+            int record_size = 20 + record.bio.length() + record.name.length();
+
+            // check for overflow condition
+            if(space != -1 && (PAGE_SIZE + pageDirectory[i_key] - space) >= record_size){
+                cout << "insert directly" << endl;
+                // insert record
+                index_file.seekp(0);
+
+                //cout << pageDirectory[i_key] * PAGE_SIZE << endl;
+                index_file << record.id << "," << record.name << "," << record.bio << "," << record.manager_id << "&";
+
+                cout << index_file.tellp();
+                // point to next page to write on
+                nextFreePage++;
+
+            // page exist but overflow in the bucket
+            }else{
+                cout << "write to new block" << endl;
+                exit(-1);
+            }
         }
-//        cout << " all key bits: "<< all_bits.to_string() << " " << i << " bits key: "<< b_key.to_string() << endl;
-        long unsigned int i_key = b_key.to_ulong();
-        cout <<  "index key: "<< i_key << endl;
-
-
-        // insert record to bucket
-        index_file.seekp(i_key * PAGE_SIZE);
-        //cout << pageDirectory[i_key] * PAGE_SIZE << endl;
-        index_file << record.id << "," << record.name << "," << record.bio << "," << record.manager_id << "&";
-        pageDirectory[i_key] =  index_file.tellp();
-        //cout << "after pos: " << pageDirectory[i_key] << endl;
-
-
         // Take neccessary steps if capacity is reached
-
-
         numRecords++;
 
     }
@@ -102,8 +156,6 @@ public:
 
         // open files
         ifstream infile;
-        //ofstream index_file;
-        //index_file.open(fName);
         infile.open(csvFName);
 
         // split data
